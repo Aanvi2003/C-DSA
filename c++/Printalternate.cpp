058ada39c5c6ffc725f78dfe7c3c65867cc050b9
Aanvi Sharma <sharmaaanvi987@gmail.com>
2026-02-09 22:14:39 +0530

    DSA

---
+++ c++/Printalternate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={10,20,30,40,50,60};
+    int n=6;
+
+    for(int i=0; i<n; i+=2)
+    {
+    cout<<arr[i]<<" ";
+    
+}
+return 0;
+}
---
+++ c++/search.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, key;
+    bool found = false;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at index: " << i << endl;
+            found = true;
+            break;   // first occurrence
+        }
+    }
+
+    if (!found) {
+        cout << "Element not found";
+    }
+
+    return 0;
+}
